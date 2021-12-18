9563e6309ff3c2bf41664ac41db72d7b5782071f
Dante Campregher <dante.campregher@student.uva.nl>
2021-12-18 21:06:48 +0100

    encoder fix and cleanup

--- encoder.py
+++ encoder.py
@@ -1,9 +1,8 @@
-# (DOES NOT) WORK IN PROGRESS
 from PIL import Image
 import argparse
 from random import randrange
 
-# Parse arguments.
+# Parse arguments
 argparser = argparse.ArgumentParser(
     description="text + image to image steganography encoder")
 argparser.add_argument("-it", "--text", type=str, default=None,
@@ -15,6 +14,7 @@ argparser.add_argument("-o", "--output", type=str, default="output.png",
 
 args = argparser.parse_args()
 
+# Check for sufficient user input
 if not args.text:
     print("No input text file")
     exit()
@@ -22,6 +22,7 @@ if not args.image:
     print("No input image file")
     exit()
 
+# Open text file
 f = open(args.text, "r")
 flength = len(f.read())
 f.seek(0, 0)
@@ -29,42 +30,37 @@ f.seek(0, 0)
 
 with Image.open(args.image).convert('RGB') as im:
 
-    im2 = Image.new(im.mode, im.size)
-    olddata = list(im.getdata())      # Contains the original image data
-    newdata = []                    # Contains the new data to put in the image
+    # Convert the image data to a list so it can ve edited
+    imglist = [list(i) for i in im.getdata()]
 
-    for p_index in range(0, len(olddata), 3):
+    for p_index in range(0, min(flength * 3, len(imglist)), 3):
 
         # Convert character to binary string
         bstring = bin(int.from_bytes(f.read(1).encode(), 'big'))[2:]
+        bstring = (8 - len(bstring)) * '0' + bstring
 
         # Loop through 3 rgb tuples in idata
         for i in range(0,3):
-
-            # Will be converted to a tuple with the RGB values of one pixel
-            new_rgb = []
-
             try:
-                # Loop through the three color bands of this pixel
-                for j, band in enumerate(olddata[p_index + i]):
+                # Loop through the three color bands of
+                # this pixel and adjust if necessary
+                for j, band in enumerate(imglist[p_index + i]):
+
                     if j + i * 3 >= len(bstring):
-                        new_rgb.append(olddata[p_index + i][j])
                         continue
-                    if band != int(bstring[j + i * 3]):
+
+                    if band % 2 != int(bstring[j + i * 3]):
                         if band == 255:
-                            new_rgb.append(olddata[p_index + i][j] - 1)
-                        if band == 0:
-                            new_rgb.append(olddata[p_index + i][j] + 1)
+                            imglist[p_index + i][j] -= 1
+                        elif band == 0:
+                            imglist[p_index + i][j] += 1
                         else:
-                            new_rgb.append(olddata[p_index + i][j]
-                            + randrange(-1, 2, 2))
+                            imglist[p_index + i][j] += randrange(-1, 2, 2)
             except IndexError:
                 break
 
-            # Append a tuple with this pixel's rgb values to the list
-            newdata.append(tuple(new_rgb))
-
     # Save file
-    im2.putdata(newdata)
+    im2 = Image.new(im.mode, im.size)
+    im2.putdata([tuple(i) for i in imglist])
     im2.save(args.output)
 
